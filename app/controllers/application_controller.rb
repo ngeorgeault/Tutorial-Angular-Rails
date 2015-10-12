e332ed6e8217fa93a3ec1b848a5062d4edd92eeb
robertmackness <robert.mackness.gbr@gmail.com>
2015-10-12 17:27:46 +0100

    Migrate USER table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Before all controller actions, demand authentication via Devise. This is due to this being an internal
+  # facing web app
+  before_action :authenticate_user!
+
 end

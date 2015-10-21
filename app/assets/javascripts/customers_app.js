9cbaf068add704ce95c7ac86e44fe63de03e6694
robertmackness <robert.mackness.gbr@gmail.com>
2015-10-21 21:00:34 +0100

    Add CustomerDetailController, $location and ngRoute business logic. 1. ngRoute, angular-rails-templates, angular-route fix via bundle    update and bower installs. 2. Fix customer search controller syntax error. 3. Add empty CustomerDetailController 4. Add 'view-details' button to customer search results

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,15 +1,33 @@
 var app = angular.module( 'customers',[
                                       'ngRoute',
-                                        ] );
+                                      'templates',
+                                        ]
+);
 
-app.controller( "CustomerSearchController", 
-                    [ '$scope', '$http', 
-                          function( $scope,   $http ) {
+app.config([
+            "$routeProvider",
+    function($routeProvider) {
+
+      $routeProvider.when("/", {
+        controller:  "CustomerSearchController",
+        templateUrl: "customer_search.html"
+      });
+      $routeProvider.when("/:id", {
+        controller:  "CustomerDetailController",
+        templateUrl: "customer_detail.html"
+      });
+
+    }
+]);
+
+app.controller("CustomerSearchController", 
+                    [ '$scope', '$http', '$location',
+              function($scope,   $http ,  $location) {
 
     $scope.customers = [ ];
     var page = 0;
 
-    $scope.search = function( searchTerm ) {
+    $scope.search = function(searchTerm) {
       
       if (searchTerm.length < 3) {
         return;
@@ -26,7 +44,7 @@ app.controller( "CustomerSearchController",
                 }, 
                 function errorCallback(response) {
                   $scope.status = response.status;
-                  alert("There was a problem: " + response.status)
+                  alert("There was a problem: " + response.status);
                 });
 
     }
@@ -42,5 +60,17 @@ app.controller( "CustomerSearchController",
         page = page +1;
         $scope.search($scope.keywords);
     }
+
+    $scope.viewDetails = function(customerid) {
+      $location.path("/" + customerid);
+    }
+
   }
 ]);
+
+
+app.controller("CustomerDetailController", [
+                '$scope', '$http', '$routeParams',
+        function($scope,   $http,   $routeParams) {
+
+        }]);
